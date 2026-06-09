f3996a003c6161be73f5be854c874a0c0eab4579
anuj9999-p <anuj.shukla2024@vitstudent.ac.in>
2026-06-09 22:20:09 +0530

    Add: sort-an-array - submission-5

---
+++ Data Structures & Algorithms/sort-an-array/submission-5.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int> newarr;for(int i=0;i<nums.size();i++){newarr.push_back(nums[i]);}
+  sort(newarr.begin(),newarr.end()); return newarr;
+        
+    }
+};
