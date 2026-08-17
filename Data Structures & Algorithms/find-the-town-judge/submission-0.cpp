25141a793f453bde09795a3ef96383ac39157267
ANUJ SHUKLA <anuj.shukla2024@vitstudent.ac.in>
2026-08-17 22:12:50 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> res(n+1,0);
+        for(auto& t: trust){
+            res[t[0]]--;
+            res[t[1]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(res[i]==n-1) return i;
+        }
+        return -1;
+    }
+};
