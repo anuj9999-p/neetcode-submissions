737ee6d6c28be59eb5a9c58a546c833f2d709355
ANUJ SHUKLA <anuj.shukla2024@vitstudent.ac.in>
2026-09-03 14:48:00 +0530

    Add: transpose-matrix - submission-2

---
+++ Data Structures & Algorithms/transpose-matrix/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows=matrix.size();
+        int c=matrix[0].size();
+        vector<vector<int>> res(c,vector<int>(rows));
+        for(int i=0;i<rows;++i){
+            for(int j=0;j<c;++j){
+             res[j][i]=matrix[i][j];
+            }  
+        }
+        return res;
+    }
+};
