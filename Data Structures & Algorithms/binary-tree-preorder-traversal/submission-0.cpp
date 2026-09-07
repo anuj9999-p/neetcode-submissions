07ea8cc04787c9be064aea2fa6b617fd3815ea1a
ANUJ SHUKLA <anuj.shukla2024@vitstudent.ac.in>
2026-09-07 22:00:49 +0530

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int> res;
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        dfs(root);
+        return res;
+
+    }
+
+private:
+     void dfs(TreeNode* node){
+        if(!node) return;
+        res.push_back(node->val);
+        dfs(node->left);
+        dfs(node->right);
+     }
+};
